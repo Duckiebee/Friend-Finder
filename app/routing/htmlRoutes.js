e134646fc61d1faf20f74aa3992968b728fa66bf
Brooke <Duckiebeewarren@gmail.com>
2017-10-17 13:12:04 -0400

    fixed app.use and get

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,17 +1,19 @@
 // Pull in required dependencies
-const path = require('path');
+var path = require('path');
 
 // Export HTML routes
 module.exports = function(app) {
 	 console.log('___ENTER htmlRoutes.js___');
 
-	// Home page
-	app.get('/', function(req, res) {
-		res.sendFile(path.join(__dirname, '/../public/home.html'));
-	});
-
 	// Survey page
 	app.get('/survey', function(req, res) {
 		res.sendFile(path.join(__dirname, '/../public/survey.html'));
 	});
-};
+
+	// Home page
+	app.use('/home', function(req, res) {
+		res.sendFile(path.join(__dirname, + '/../public/home.html'));
+	});
+
+
+}
